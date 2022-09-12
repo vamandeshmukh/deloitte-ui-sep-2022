39cf2429503aa28ac2989bdf77a4cec62a588cef
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-12 19:19:29 +0530

    js basics

--- script.js
+++ script.js
@@ -78,27 +78,95 @@
 // null and undefined and NaN 
 
 
-// 
-let isMarried = true; // false // null
+// // 
+// let isMarried = true; // false // null
 
-// let a = null;
+// // let a = null;
 
-let a;
-let b = 20;
+// let a;
+// let b = 20;
 
-// console.log(a);
-// console.log(typeof(a));
+// // console.log(a);
+// // console.log(typeof(a));
 
-console.log(a + b);
+// console.log(a + b);
 
-// falsy = false, 0, null, undefined, ``
-// truthy = remaining  
+// // falsy = false, 0, null, undefined, ``
+// // truthy = remaining  
 
-let someInputValue = 0;
+// let someInputValue = 0;
 
-if (someInputValue) {
-    console.log(`value is entered.`);
+// if (someInputValue) {
+//     console.log(`value is entered.`);
+// }
+// else {
+//     console.log("value is not entered.")
+// }
+
+
+// reference datatypes in JS 
+
+// arrays 
+
+let myList = [10, 10.5, `abc`, false, null];
+// console.log(myList);
+
+// for (let item of myList) {
+//     console.log(item);
+// }
+// console.log(`access a specific element:`)
+// console.log(myList[2]);
+// console.log(myList[5]);
+// myList[5] = 25;
+// console.log(myList[5]);
+
+// objects 
+
+let empData = {
+    eid: 101,
+    firstName: `Sonu`,
+    salary: 10.5,
+    isMarried: false,
+    spouseName: null,
+    phones: [9876543210, 8532178965],
+    address: {
+        city: `Hyderabad`,
+        street: 'Bank street',
+        pin: 500001
+    }
+};
+
+// console.log(`Employee data:`);
+// console.log(empData);
+// console.log(`specific property`)
+// console.log(empData.address.city);
+// console.log(empData.phones[1]);
+
+
+// console.log(`Using object properties:`);
+// const data1 = "This employee's name is " + empData.firstName + " and their salarey is " + empData.salary + ".";
+// const data2 = `This employee's name is ${empData.firstName} and their salarey is ${empData.salary}.`;
+// console.log(data1);
+// console.log(data2);
+// console.log(typeof(data1));
+// console.log(typeof(data2));
+
+
+
+// functions in JS 
+
+
+console.log(`Start`);
+
+function fun() {
+    console.log(`fun function invoked.`);
 }
-else {
-    console.log("value is not entered.")
+fun();
+
+const fun2 = () => {
+    console.log(`fun2 function invoked.`);
 }
+
+fun2();
+
+console.log(`End`);
