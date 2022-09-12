4bf6b362669a353699276d0e08c60c7c02f9038a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-12 15:00:51 +0530

    js-intro

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-console.log('Hello world!');
 
-console.log(10 + 20);
+// // intro to JS 
+
+// console.log('Hello world!');
+// console.log('10' + 1);
+// console.log('10' - 1);
+
+// // String literals in JS  
+
+// console.log('Vaman');
+// console.log("Vaman");
+// console.log(`Vaman`); // this 
+
+// // Datatypes in JS 
+
+// // number, string, boolean 
+// // array, object, class-object, function  
+
+// variable declaration in JS 
+
+// java 
+// int num = 10;
+// JS 
+
+num = 10; // don't use this 
+var num2 = 20; // don't use this 
+
+const num4 = 40; // use this for constants (like final in java)
+let num3 = 30;  // use this  for other variables 
+
+console.log(num);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+
+
+
+
 
 
 
