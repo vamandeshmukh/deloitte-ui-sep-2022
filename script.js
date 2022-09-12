8ab2545fbf19a482359d3d9d9c098a7739256772
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-12 15:15:19 +0530

    js basics

--- script.js
+++ script.js
@@ -22,19 +22,46 @@
 // int num = 10;
 // JS 
 
-num = 10; // don't use this 
-var num2 = 20; // don't use this 
+// num = 10; // don't use this 
+// var num2 = 20; // don't use this 
 
-const num4 = 40; // use this for constants (like final in java)
-let num3 = 30;  // use this  for other variables 
+// const num4 = 40; // use this for constants (like final in java)
+// let num3 = 30;  // use this  for other variables 
 
-console.log(num);
-console.log(num2);
-console.log(num3);
-console.log(num4);
+// console.log(num);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+// JS - check the datatypes 
+
+// let a = 10;
+// let b = `abc`;
+// let c = false;
+
+// console.log(a);
+// console.log(typeof(a));
+// console.log(b);
+// console.log(typeof(b));
+// console.log(c);
+// console.log(typeof(c));
 
 
+// // const vs let 
 
+// let a = 10;
+// const b = 20;
+// console.log(a);
+// console.log(b);
+// a = 15;
+// b = 25;
+// console.log(a);
+// console.log(b);
+
+// JS - dynamically typed language 
+
+let someVariable = 10;
+console.log(someVariable);
 
 
 
