b825a86da15c191abb8d2a4f60960853be05f043
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-13 11:10:43 +0530

    js-concepts

---
+++ script2.js
@@ -0,0 +1,55 @@
+
+// variable hoisting 
+
+
+// console.log(num);
+
+// const num = 10;
+
+// console.log(num);
+
+// fun();
+
+// let fun = () => {
+//     console.log(`fun function invoked.`);
+// }
+
+// fun();
+
+// let a = 5;
+// a += 10;
+// a = a + 10;
+
+
+
+// operators   == and === 
+
+// console.log(1 == true);
+// console.log(10 == `10`); // 10 != 10 
+// console.log(10 === `10`); // number != string 
+
+// console.log(10 != `10`); // 10 == 10 
+// console.log(10 !== `10`); // number == string 
+
+// functions with / without args and return values 
+
+
+// const fun = (num1, num2) => {
+//     console.log(num1 + num2);
+// }
+
+// fun(10);
+// fun(10, 20);
+// fun(10, 20, 30);
+
+const fun2 = (num1, num2) => {
+    return num1 + num2;
+}
+
+fun2(10);
+fun2(10, 20);
+fun2(10, 20, 30);
+
+
+
+
