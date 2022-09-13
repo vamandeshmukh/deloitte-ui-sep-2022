bb05755d21f64ebab59b1f151bb26020d7bd1d69
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-13 14:16:46 +0530

    js-concepts

--- script3.js
+++ script3.js
@@ -6,13 +6,29 @@
 
 // Using callbacks in JS 
 
+// console.log(`One`);
+
+// setTimeout(() => {
+//     console.log('Two');
+// }, 2000);
+
+// console.log("Three");
+
+
 console.log(`One`);
 
-setTimeout(() => {
-    console.log('Two');
-}, 2000);
+const fun = (callBack) => {
+    setTimeout(() => {
+        console.log('Two');
+        callBack(`Three`); // function call 
+    }, 2000);
+};
 
-console.log("Three");
+fun((arg) => {
+    console.log(arg);
+});
 
+// same as above, but shorter 
+// fun(arg => console.log(arg));
 
-
+// console.log("Three");
