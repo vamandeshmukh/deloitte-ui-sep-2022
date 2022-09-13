d9d43fe0a19a99bb900a1bccceea69a4c00bd180
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-13 14:30:54 +0530

    js-concepts

--- script3.js
+++ script3.js
@@ -15,20 +15,52 @@
 // console.log("Three");
 
 
-console.log(`One`);
+// console.log(`One`);
 
-const fun = (callBack) => {
-    setTimeout(() => {
-        console.log('Two');
-        callBack(`Three`); // function call 
-    }, 2000);
+// const fun = (callBack) => {
+//     setTimeout(() => {
+//         console.log('Two');
+//         callBack(`Three`); // function call 
+//     }, 2000);
+// };
+
+// fun((arg) => {
+//     console.log(arg);
+// });
+
+// // same as above, but shorter 
+// // fun(arg => console.log(arg));
+
+
+
+// console.log(this);
+
+
+
+let employeeData = {
+    eid: 101,
+    firstName: `Sonu`,
+    salary: 10.5,
+    isMarried: false,
+    spouseName: null,
+    phones: [9876543210, 8532178965],
+    address: {
+        city: `Hyderabad`,
+        street: 'Bank street',
+        pin: 500001
+    }
 };
 
-fun((arg) => {
-    console.log(arg);
-});
-
-// same as above, but shorter 
-// fun(arg => console.log(arg));
 
 
+// for (a in employeeData)
+//     console.log(a);
+
+// for (a of employeeData)
+//     console.log(a);
+
+const keysArray = Object.keys(employeeData);
+console.log(keysArray);
+
+const valuesArray = Object.values(employeeData);
+console.log(valuesArray);
